54a0eff7701606cac35ffb64555cb82cf180beb7
Joshua M Loran <joshua.loran@gmail.com>
2019-01-29 18:50:23 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -21,7 +21,7 @@ def get_japanese_emoticon(file_path, emoticon)
   end
 end
 
-def get_english_meaning
+def get_english_meaning(file_path, emoticon)
   emoticon_hash = load_library(file_path)
   result = emoticon_hash["get_meaning"][emoticon]
   if result
